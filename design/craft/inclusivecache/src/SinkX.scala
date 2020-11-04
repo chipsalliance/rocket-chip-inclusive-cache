a18f24a983e9aad9e6ce72861364ed1a5a81ec3b
Jiuyang liu <liujiuyang1994@gmail.com>
2020-11-04 08:27:02 +0000

    add doc to SinkX.

--- design/craft/inclusivecache/src/SinkX.scala
+++ design/craft/inclusivecache/src/SinkX.scala
@@ -25,27 +25,40 @@ class SinkXRequest(params: InclusiveCacheParameters) extends InclusiveCacheBundl
   val address = UInt(width = params.inner.bundle.addressBits)
 }
 
+/** Interface between MSHR and Sink X.
+  * Receive request form flush controller,
+  * convert it to MSHR protocol.
+  */
 class SinkX(params: InclusiveCacheParameters) extends Module
 {
   val io = new Bundle {
+    /** Outward to MSHR with its own protocol. */
     val req = Decoupled(new FullRequest(params))
+    /** from flush controller, requ to flush a specific cacheline. */
     val x = Decoupled(new SinkXRequest(params)).flip
   }
 
+  /** Buffer request from controller. */
   val x = Queue(io.x, 1)
+  /** convert address to [[tag]], [[set]] and [[offset]]. */
   val (tag, set, offset) = params.parseAddress(x.bits.address)
 
+  /* couple ready valid interface. */
   x.ready := io.req.ready
   io.req.valid := x.valid
   params.ccover(x.valid && !x.ready, "SINKX_STALL", "Backpressure when accepting a control message")
 
-  io.req.bits.prio   := Vec(UInt(1, width=3).asBools) // same prio as A
+  /* same priority as A */
+  io.req.bits.prio   := Vec(UInt(1, width=3).asBools)
+  /* indicate this transaction is from control. */
   io.req.bits.control:= Bool(true)
+  /* Don't care about opcode etc. */
   io.req.bits.opcode := UInt(0)
   io.req.bits.param  := UInt(0)
   io.req.bits.size   := UInt(params.offsetBits)
-  // The source does not matter, because a flush command never allocates a way.
-  // However, it must be a legal source, otherwise assertions might spuriously fire.
+  /* The source does not matter, because a flush command never allocates a way.
+   * However, it must be a legal source, otherwise assertions might spuriously fire.
+   */
   io.req.bits.source := UInt(params.inner.client.clients.map(_.sourceId.start).min)
   io.req.bits.offset := UInt(0)
   io.req.bits.set    := set
