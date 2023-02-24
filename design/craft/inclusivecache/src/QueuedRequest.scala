01e47fbbd09bea6e6d6884e93653a75553e4724e
Ocean Shen <30361859+OceanS2000@users.noreply.github.com>
2023-02-24 20:19:04 +0800

    port QueuedRequest.scala to chisel3

--- design/craft/inclusivecache/src/QueuedRequest.scala
+++ design/craft/inclusivecache/src/QueuedRequest.scala
@@ -17,24 +17,24 @@
 
 package sifive.blocks.inclusivecache
 
-import Chisel._
+import chisel3._
 
 class QueuedRequest(params: InclusiveCacheParameters) extends InclusiveCacheBundle(params)
 {
   val prio   = Vec(3, Bool()) // A=001, B=010, C=100
   val control= Bool() // control command
-  val opcode = UInt(width = 3)
-  val param  = UInt(width = 3)
-  val size   = UInt(width = params.inner.bundle.sizeBits)
-  val source = UInt(width = params.inner.bundle.sourceBits)
-  val tag    = UInt(width = params.tagBits)
-  val offset = UInt(width = params.offsetBits)
-  val put    = UInt(width = params.putBits)
+  val opcode = UInt(3.W)
+  val param  = UInt(3.W)
+  val size   = UInt(params.inner.bundle.sizeBits.W)
+  val source = UInt(params.inner.bundle.sourceBits.W)
+  val tag    = UInt(params.tagBits.W)
+  val offset = UInt(params.offsetBits.W)
+  val put    = UInt(params.putBits.W)
 }
 
 class FullRequest(params: InclusiveCacheParameters) extends QueuedRequest(params)
 {
-  val set = UInt(width = params.setBits)
+  val set = UInt(params.setBits.W)
 }
 
 class AllocateRequest(params: InclusiveCacheParameters) extends FullRequest(params)
