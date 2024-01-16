ea366d74b13ff2b0d91d6fdfe64001148311a2f5
Tynan McAuley <tynan@niobiummicrosystems.com>
2024-01-16 12:05:01 -0800

    Update deprecated Chisel APIs
    These have been deprecated since Chisel 3.6.0. They are being removed in
    Chisel 6, and will become compile errors at that point.

--- design/craft/inclusivecache/src/Parameters.scala
+++ design/craft/inclusivecache/src/Parameters.scala
@@ -19,7 +19,7 @@ package sifive.blocks.inclusivecache
 
 import chisel3._
 import chisel3.util._
-import chisel3.internal.sourceinfo.SourceInfo
+import chisel3.experimental.SourceInfo
 import org.chipsalliance.cde.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
